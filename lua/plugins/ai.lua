4f65fa3a74da56f677caedbf331141c92fd279d7
Brandon Barker <brandon.barker@mechanicalrock.io>
2024-09-25 20:44:34 +0800

    cleanup

--- lua/plugins/ai.lua
+++ lua/plugins/ai.lua
@@ -2,7 +2,7 @@ return {
   {
     "yetone/avante.nvim",
     event = "VeryLazy",
-    lazy = true,
+    lazy = false,
     opts = {
       provider = "copilot"
     },
@@ -19,14 +19,12 @@ return {
         "HakonHarnes/img-clip.nvim",
         event = "VeryLazy",
         opts = {
-          -- recommended settings
           default = {
             embed_image_as_base64 = false,
             prompt_for_file_name = false,
             drag_and_drop = {
               insert_mode = true,
             },
-            -- required for Windows users
             use_absolute_path = true,
           },
         },
